8eb29413c8c49b9e359d9b2db3232232b430bdd4
Daniel <danielrosabarcellos17@gmail.com>
2024-07-19 17:36:58 -0300

    Caixa eletronico finalizado

--- Laço While/Caixa_eletronico.py
+++ Laço While/Caixa_eletronico.py
@@ -1,15 +1,50 @@
 from colorama import Fore, Style
 import time
 
+# Definindo cores
 red = Fore.RED
 blue = Fore.BLUE
 green = Fore.GREEN
 reset = Style.RESET_ALL
 
-pausa = time.sleep(1)
-
-
+# Cabeçalho do caixa eletrônico
 print(f'{green}--' * 30)
-print('------------------ CAIXA ELETRONICO ------------------')
-print('--' * 30, reset) 
+print('------------------ CAIXA ELETRÔNICO ------------------')
+print('--' * 30)
+print('')
 
+# Variáveis iniciais
+cedulas = [50, 20, 10, 1]
+nome = ''
+
+# Validação do nome
+while not nome.strip() or not all(char.isalpha() or char.isspace() for char in nome):
+    nome = input('Digite seu nome: ').strip()
+    if not nome:
+        print(f'{red}É necessário digitar seu nome.{reset}')
+    elif not all(char.isalpha() or char.isspace() for char in nome):
+        print(f'{red}Digite um nome válido (apenas letras e espaços são permitidos).{reset}')
+    
+print(f'{green}Olá {reset}{nome}!')
+
+# Solicitação do valor de saque
+while True:
+    try:
+        valor = int(input(f'{green}Informe o valor a ser sacado: {reset}'))
+        if valor <= 0:
+            raise ValueError
+        break
+    except ValueError:
+        print(f'{red}Por favor, insira um valor inteiro positivo.{reset}')
+
+# Processamento das cédulas
+print(f'\n{blue}Contagem das cédulas:{reset}')
+for ced in cedulas:
+    total_cedula = 0
+    while valor >= ced:
+        valor -= ced
+        total_cedula += 1
+    if total_cedula > 0:
+        print(f'Total de {total_cedula} cédulas de R$ {ced}')
+
+print(f'\n{green}Saque concluído!{reset}')
