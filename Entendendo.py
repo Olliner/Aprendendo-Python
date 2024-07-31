4ebf17cb0fd7af747eab679adf889fbbbc22d931
Daniel <danielrosabarcellos17@gmail.com>
2024-07-31 15:29:58 -0300

    Entendendo Tuplas

---
+++ Entendendo.py
@@ -0,0 +1,39 @@
+
+# pessoa = 'Daniel', 23, 'M', 65
+# print(pessoa)
+
+
+
+
+
+# a = 2, 5, 4
+# b= 5, 8, 1, 2
+
+# c = a + b
+# print(c)
+# print(c.index(8))
+# print(c.count(4))
+
+
+
+
+# lanche = 'Hambúrguer', 'Suco', 'Pizza', 'Pudim'
+
+
+
+
+
+# Coloca em ordem
+# print(sorted(lanche))
+
+# Duas formas de utilizar o for com uma tupla
+
+# for cont in range(0 , len(lanche)):
+#     print(lanche[cont],'na posição ',cont)
+
+# for pos, comida in enumerate(lanche):
+#     print(f'Comer {comida} na posição {pos}')
+
+# for c in lanche:
+#     print(f'Eu vou comer {c}')
+# print('Comi muito!')
